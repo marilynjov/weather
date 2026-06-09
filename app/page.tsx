8e15c976175018c34c2ae87e21c7d6882b15806e
Marilyn Joven <m.joven@uniandes.edu.co>
2026-06-08 22:14:01 -0500

    setters

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,32 @@
 "use client";
 
-import React from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 
 let title: string = "Weather App";
 
 type ButtonProps = {
-  children: React.ReactNode; 
-  // JSX.Element; from React 18, ReactNode is more appropriate for 
-  // children as it can represent any renderable content, including 
-  // strings, numbers, fragments, and arrays of elements.
+  setCount: Dispatch<SetStateAction<number>>
 }
 
-function Button({children}: ButtonProps){
+function Button({setCount}: ButtonProps){
+
   return (
-    <button >
+    <button onClick = {() => setCount(5)}>
       {title} 
     </button>
   );
 }
 
 function Page() {
+  const[count, setCount] = useState<number>(0)
+  
   return (
     <div>
-      <Button>
-          Click
-      </Button>
+      <h1 style={{ textAlign: "center" }}>{count}</h1>
+      <Button
+        setCount = {setCount}
+      />
+          
     </div>
   );
 }
