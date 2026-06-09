e83ea1c8a40a8c696b6122076f42b5b8e205a34b
Marilyn Joven <m.joven@uniandes.edu.co>
2026-06-08 19:25:13 -0500

    prop types

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-function name(params:type) {
-  
+
+let title: string = "Weather App";
+
+type ButtonProps = {
+  text: string;
+  color?: string;
 }
+const Button = (props: ButtonProps) => {
+  const { text, color } = props;
+  return (
+    <button>
+      {text} 
+    </button>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Button text = {title} color = "orange"/>
+    </div>
+  );
+}
+
+export default Page;
